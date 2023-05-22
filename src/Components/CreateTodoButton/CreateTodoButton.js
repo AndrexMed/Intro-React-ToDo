90092e399776ff9ac0f4f5bd1ea0c80308c0aaf4
AndrexMed <giovanniandrex@gmail.com>
2023-05-22 16:30:56 -0500

    Eventos en React, onClick & onChange

--- src/Components/CreateTodoButton/CreateTodoButton.js
+++ src/Components/CreateTodoButton/CreateTodoButton.js
@@ -2,9 +2,14 @@ import './CreateTodoButton.css'
 
 function CreateTodoButton() {
     return (
-        <button className='botonAñadirTarea'>
+        <button className='botonAñadirTarea'
+            onClick={(evento) => {
+                console.log("Hello")
+                console.log(evento)
+                console.log(evento.target)
+            }}>
             +
-            </button>
+        </button>
     );
 }
 export { CreateTodoButton }
--- src/Components/ToDoSearch/TodoSearch.js
+++ src/Components/ToDoSearch/TodoSearch.js
@@ -4,7 +4,13 @@ function TodoSearch() {
   return (
     <div className='container-search'>
       <input placeholder="Añadir Tarea, Ex: Cortar Cebolla"
-        className='TodoSearch'>
+        className='TodoSearch'
+        onChange={(evento) => {
+          console.log("Escribiste en el Search...")
+          console.log(evento)
+          console.log(evento.target)
+          console.log(evento.target.value);
+        }}>
       </input>
     </div>
 
